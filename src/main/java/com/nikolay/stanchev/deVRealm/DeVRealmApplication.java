69752991d646e52db839efd71734cca133e529da
NikiStanchev <nikolay_stanchev@mail.com>
2020-04-13 15:46:36 +0300

    init The Country Neighbours Tour App

---
+++ src/main/java/com/nikolay/stanchev/deVRealm/DeVRealmApplication.java
@@ -0,0 +1,13 @@
+package com.nikolay.stanchev.deVRealm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeVRealmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeVRealmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikolay/stanchev/deVRealm/DeVRealmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikolay.stanchev.deVRealm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeVRealmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
